b4f5186d29b0ff046bb9016d0d4152498cb693c7
Kristján Oddsson <koddsson@gmail.com>
2020-10-26 13:34:18 +0000

    Move data fetching of posts into own module

--- koddsson.com/src/app.ts
+++ koddsson.com/src/app.ts
@@ -1,17 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
-import relativeDate from 'relative-date'
 import hbs from 'hbs'
 import {fileURLToPath} from 'url'
 import {dirname} from 'path'
 import markdown from 'helper-markdown'
-import type {Note, Photo} from './types'
 
 import micropub from './micropub.js'
 import favorites from './favorites.js'
 import notes from './notes.js'
-import * as db from './database.js'
+import {getLatestPost} from './data.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -31,27 +29,6 @@ hbs.registerPartials(__dirname + '/views/partials')
 
 // Set up markdown helper in handlebar views.
 hbs.registerHelper('markdown', markdown({linkify: true}))
-
-async function getPhoto(slug: string): Promise<Photo> {
-  return (await db.get<Photo>('SELECT * FROM photos where slug = ?', slug))!
-}
-
-async function getLatestPost() {
-  // TODO: Somehow get favorites to show up in the latest notes again
-  // SELECT url as content, slug, 'favorite' as type, slug as timestamp, NULL as reply from favorites UNION ALL
-  const latestNote = await db.get<Note>(`
-    SELECT content, slug, 'note' as type, timestamp, replyTo as reply FROM notes
-    ORDER BY timestamp DESC
-    LIMIT 1
-  `)
-  if (latestNote) {
-    latestNote.relativeDate = relativeDate(latestNote.timestamp * 1000)
-    latestNote.isNote = true
-    latestNote.isFavorite = false
-    latestNote.photo = await getPhoto(latestNote.slug)
-  }
-  return latestNote
-}
 
 app.get('/', async (_, res) => res.render('index', {latestNote: await getLatestPost()}))
 
---
+++ koddsson.com/src/data.ts
@@ -0,0 +1,30 @@
+import relativeDate from 'relative-date'
+
+import type {Note, Photo} from './types'
+import {get} from './database'
+
+async function getPhoto(slug: string): Promise<Photo> {
+  return (await get<Photo>('SELECT * FROM photos where slug = ?', slug))!
+}
+
+async function getLatestNote() {
+  const latestNote = await get<Note>(`
+    SELECT content, slug, 'note' as type, timestamp, replyTo as reply FROM notes
+    ORDER BY timestamp DESC
+    LIMIT 1
+  `)
+  if (latestNote) {
+    latestNote.relativeDate = relativeDate(latestNote.timestamp * 1000)
+    latestNote.isNote = true
+    latestNote.isFavorite = false
+    latestNote.photo = await getPhoto(latestNote.slug)
+  }
+  return latestNote
+}
+
+export async function getLatestPost() {
+  const latestNote = await getLatestNote()
+  // TODO: Somehow get favorites to show up in the latest notes again
+  // SELECT url as content, slug, 'favorite' as type, slug as timestamp, NULL as reply from favorites UNION ALL
+  return latestNote
+}
