2cd5437484f08b909b34a8366ac2a19aec90255a
Kristján Oddsson <koddsson@gmail.com>
2020-10-30 14:09:55 +0000

    take src/ back out of views path

--- koddsson.com/src/favorites.ts
+++ koddsson.com/src/favorites.ts
@@ -11,7 +11,7 @@ const app = express()
 
 app.set('view engine', 'hbs')
 
-app.set('views', __dirname + '/src/views')
+app.set('views', __dirname + '/views')
 hbs.registerPartials(__dirname + '/src/views/partials')
 
 app.get('/', async (_, res) => res.render('favorites', {favorites: await getFavorites()}))
